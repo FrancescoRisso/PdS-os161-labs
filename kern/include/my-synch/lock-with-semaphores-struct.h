d25fb2a4a7f0d4226095d4716f6746fd24c8e8ea
Francesco Risso <francesco@frisso.net>
2024-05-10 11:05:08 +0000

    Separated struct definition for locks

---
+++ kern/include/my-synch/lock-with-semaphores-struct.h
@@ -0,0 +1,14 @@
+#include <spinlock.h>
+
+#include "opt-lock_with_sems.h"
+
+#if OPT_LOCK_WITH_SEMS
+
+struct lock {
+	char *lk_name;
+	HANGMAN_LOCKABLE(lk_hangman); /* Deadlock detector hook. */
+								  // add what you need here
+								  // (don't forget to mark things volatile as needed)
+};
+
+#endif
