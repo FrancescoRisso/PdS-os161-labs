e8225e600875f673be70e06733720feba4758d56
Francesco Risso <francesco@frisso.net>
2024-05-10 14:22:10 +0000

    Separated default cv to dedicated files

---
+++ kern/include/my-synch/cv-default-struct.h
@@ -0,0 +1,10 @@
+#ifndef _CV_DEFAULT_STRUCT
+#define _CV_DEFAULT_STRUCT
+
+struct cv {
+	char *cv_name;
+	// add what you need here
+	// (don't forget to mark things volatile as needed)
+};
+
+#endif
---
+++ kern/thread/my-synch/cv-default.c
@@ -0,0 +1,54 @@
+#include <__includeTypes.h>
+#include <lib.h>
+#include <synch.h>
+
+/*
+#include <current.h>
+#include <spinlock.h>
+#include <thread.h>
+#include <wchan.h>
+*/
+
+struct cv *cv_create(const char *name) {
+	struct cv *cv;
+
+	cv = kmalloc(sizeof(*cv));
+	if(cv == NULL) { return NULL; }
+
+	cv->cv_name = kstrdup(name);
+	if(cv->cv_name == NULL) {
+		kfree(cv);
+		return NULL;
+	}
+
+	// add stuff here as needed
+
+	return cv;
+}
+
+void cv_destroy(struct cv *cv) {
+	KASSERT(cv != NULL);
+
+	// add stuff here as needed
+
+	kfree(cv->cv_name);
+	kfree(cv);
+}
+
+void cv_wait(struct cv *cv, struct lock *lock) {
+	// Write this
+	(void) cv;    // suppress warning until code gets written
+	(void) lock;  // suppress warning until code gets written
+}
+
+void cv_signal(struct cv *cv, struct lock *lock) {
+	// Write this
+	(void) cv;    // suppress warning until code gets written
+	(void) lock;  // suppress warning until code gets written
+}
+
+void cv_broadcast(struct cv *cv, struct lock *lock) {
+	// Write this
+	(void) cv;    // suppress warning until code gets written
+	(void) lock;  // suppress warning until code gets written
+}
